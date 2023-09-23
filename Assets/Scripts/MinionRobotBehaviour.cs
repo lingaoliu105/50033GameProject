b22c2416fda01dc77c1b3c155c3e8a273642b83c
Liu Lingao <lingaoliu105@gmail.com>
2023-09-23 14:30:18 +0800

    enemy design basic

---
+++ Assets/Scripts/MinionRobotBehaviour.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinionRobotBehaviour : MonoBehaviour,EnemyBehaviourInterface
+{
+    public void Move()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public float hp { get; set; }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PrimaryAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
