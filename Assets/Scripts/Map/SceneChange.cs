7a2bd514eb7bccf3441ebd6a547cab43afd97083
Melt628 <Meltryllis628@gmail.com>
2023-12-06 00:18:33 +0800

    Trigger

---
+++ Assets/Scripts/Map/SceneChange.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Assets.Scripts.Map {
+    public class SceneChange:MapObjectController {
+        public int sceneId;
+        public override void Interact(int triggerId) {
+            GameManager.Instance.ChangeScene(sceneId);
+        }
+    }
+}
